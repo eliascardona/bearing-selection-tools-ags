5deacb7f6fdf2b21319ff59019f85694ac57bb41
Elias Cardona <eliascardonarod@gmail.com>
2023-10-21 14:54:38 -0600

    DIA 21 OCT 2023. Subiendo nuestro proyecto al repositorio

--- src/bearingselectiontools_backend/main.mo
+++ src/bearingselectiontools_backend/main.mo
@@ -1,5 +1,96 @@
-actor {
-  public query func greet(name : Text) : async Text {
-    return "Hello, " # name # "!";
-  };
+import Float "mo:base/Float";
+import Debug "mo:base/Debug";
+import HashMap "mo:base/HashMap";
+import Nat "mo:base/Nat";
+import Hash "mo:base/Hash";
+import Iter "mo:base/Iter";
+
+actor bearingselectiontools {
+
+	stable var cId : Nat = 0;
+	stable var elementoId : Nat = 0;
+
+	var calculoRodamiento : Float = 0.0;
+
+
+	type Requerimientos = {
+		vidaUtil: Float;
+		cargaAxial: Float;
+		cargaRadial: Float;
+		revoluciones: Float;
+	};
+
+	type miStruct = {
+		rodNumeros: Nat;
+		mmd: Nat;
+        mmD: Nat;
+        mmB: Nat;
+		r: Nat;
+        estaticaCO: Nat;
+        dinamicaC: Nat;
+        velMax: Float;
+	};
+
+	var calculos = HashMap.HashMap<Nat, Requerimientos>(0, Nat.equal, Hash.hash);
+	var miArreglo = HashMap.HashMap<Nat, miStruct>(10, Nat.equal, Hash.hash);
+
+
+	// public func calcularRodamiento(elem : Requerimientos) : ?Float {
+
+	// 	let id : Nat = cId;
+	// 	cId+=1;
+
+	// 	calculos.put(id, elem);
+
+	// 	Debug.print(debug_show(elem.vidaUtil));
+
+
+	// 	// variables para manejar los cálculos
+
+	// 	var varL : Float = elem.vidaUtil*revoluciones;
+
+	// 	var fuerzauno : Float = 1*elem.cargaRadial + 2*elem.cargaAxial;
+	// 	var fuerzados : Float = 0.5*elem.cargaRadial + 1.4*elem.cargaAxial;
+
+	// 	if(fuerzauno > fuerzados) {
+	// 		var fElegida : Float = fuerzauno;
+	// 	} else if(fuerzauno < fuerzados) {
+	// 		fElegida := fuerzados;
+	// 	}
+		
+	// 	var rParcial : Float = elem.vidaUtil*1000000/1000000;
+	// 	var raiz : Float = resParcial**(1/3);
+
+	// 	var calculoRodamiento : Float = fElegida*raiz;
+
+	// 	return calculoRodamiento;
+
+	// };
+
+
+	public func crearBaseDeDatos (elemento : miStruct) : async () {
+
+		let id : Nat = elementoId;
+		elementoId+=1;
+
+
+		miArreglo.put(id, elemento);
+
+
+		return ();
+
+	};
+
+
+	public func buscarRodamiento () : () {
+
+		let miArregloIter : Iter.Iter<(Nat, miStruct)> = miArreglo.entries();
+		
+		let arregloFormateado : [(Nat, miStruct)] = Iter.toArray(miArregloIter);
+
+
+		return ();
+
+	};
+
 };
--- src/bearingselectiontools_frontend/src/index.js
+++ src/bearingselectiontools_frontend/src/index.js
@@ -1,19 +1,73 @@
-import { bearingselectiontools_backend } from "../../declarations/bearingselectiontools_backend";
+import { bearingselectiontools_backend } from "../../declarations/bearingselectiontools_backend"
+//import { AuthClient } from "@dfinity/auth-client"
+//import { HttpAgent } from "@dfinity/agent"
+//let actor = bearingselectiontools_backend
 
-document.querySelector("form").addEventListener("submit", async (e) => {
-  e.preventDefault();
-  const button = e.target.querySelector("button");
+const editorBtn = document.getElementById("abrir-editor")
+const cerrarEditor = document.getElementById("cerrar-editor")
+const modalEditor = document.getElementById("modal-editor")
+const formUno = document.getElementById("form-uno")
 
-  const name = document.getElementById("name").value.toString();
 
-  button.setAttribute("disabled", true);
+editorBtn.addEventListener("click", () => {
+	modalEditor.style.display="block"
+})
 
-  // Interact with foo actor, calling the greet method
-  const greeting = await bearingselectiontools_backend.greet(name);
 
-  button.removeAttribute("disabled");
+cerrarEditor.addEventListener("click", () => {
+	modalEditor.style.display="none"
+})
 
-  document.getElementById("greeting").innerText = greeting;
 
-  return false;
-});
+formUno.addEventListener("submit", async (e) => {
+	e.preventDefault()
+	const crearData = new FormData(e.target)
+
+	const dato1 = crearData.get("dato1")
+	const dato2 = crearData.get("dato2")
+	const dato3 = crearData.get("dato3")
+	const dato4 = crearData.get("dato4")
+
+	const Front = {
+		vidaUtil: parseInt(dato1),
+		cargaAxial: parseInt(dato2),
+		cargaRadial: parseInt(dato3),
+		revoluciones: parseInt(dato4),
+	}
+	console.log("objeto enviado")
+	console.log(Front)
+
+	await bearingselectiontools_backend.calcularRodamiento(Front)
+})
+
+
+//loginBtn.addEventListener("click", async (e) => {
+//	e.preventDefault()
+//	console.log("identificando...")
+//
+//	let authClient = await AuthClient.create()
+//
+//	await new Promise((resolve, reject) => {
+//		authClient.login({
+//			identityProvider: process.env.INTERNET_IDENTITY_URL,
+//			onSuccess: () => {
+//				console.log("Identificación exitosa")
+//				resolve()
+//			},
+//			onError: (error) => {
+//				console.error("Error on Internet Identity 'auth/client' process:", error)
+//				reject(error)
+//			},
+//		})
+//	})
+//
+//	const identity = authClient.getIdentity()
+//
+//	const agent = new HttpAgent({identity})
+//
+//	actor = createActor(process.env.DECENTRALIZEDGYM_BACKEND_CANISTER_ID, {
+//		agent,
+//	})
+//
+//	return false
+//})
